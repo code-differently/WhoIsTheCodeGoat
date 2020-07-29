29f3e67353b7a4afc60556e9d7d87d8fd94eddf9
Kaveesha <weerasirikaveesha@gmail.com>
2020-07-29 18:55:51 -0500

    finished problem 3

--- src/main/java/Problem03.java
+++ src/main/java/Problem03.java
@@ -1,2 +1,30 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class Problem03 {
+    public String countKLengthStringsWithKDistinctLetters(String input, int k) {
+        int numberOfCorrectStrings = 0, copyOfK = k;
+        String subString = "";
+        for(int index = 0; index < input.length(); index++) {
+            subString = copyOfK > input.length() ? "" : input.substring(index, copyOfK);
+            numberOfCorrectStrings = isSubstringUnique(subString) && !subString.equals("") ? numberOfCorrectStrings + 1 : numberOfCorrectStrings;
+            copyOfK++;
+        }
+
+        return String.valueOf(numberOfCorrectStrings);
+    }
+
+    private boolean isSubstringUnique(String subString) {
+        Set<Character> seenCharacterBefore = new HashSet<>();
+
+        for(Character letter: subString.toCharArray()) {
+            if(!seenCharacterBefore.contains(letter)) {
+                seenCharacterBefore.add(letter);
+            }
+            else {
+                return false;
+            }
+        }
+        return true;
+    }
 }
