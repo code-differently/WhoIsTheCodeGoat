26e9ae983898f6ca5bafc18a0feae9e3b97f672c
Kaveesha <weerasirikaveesha@gmail.com>
2020-07-29 18:04:35 -0500

    finished first problem

--- src/main/java/Problem01.java
+++ src/main/java/Problem01.java
@@ -1,2 +1,19 @@
 public class Problem01 {
+    public String computeAllPrimesSmallerThanEqualToN(int value) {
+        StringBuilder primes = new StringBuilder();
+
+        for(int index = 2; index <= value; index++) {
+            checkNumbersUnder10(primes, index);
+            primes.append(index > 10 && checkIfPrimeOver10(index) ? index + " " : "");
+        }
+        return primes.toString().substring(0, primes.length() - 1); //takes out the last space
+    }
+
+    private void checkNumbersUnder10(StringBuilder primes, int index) {
+        primes.append(index == 2 || index == 3 || index == 5 || index == 7 ? index + " " : "");
+    }
+
+    private boolean checkIfPrimeOver10(int index) {
+        return index % 2 != 0 && index % 3 != 0 && index % 5 != 0 && index % 7 != 0; //not divisible by 2,3,5,7 then return true
+    }
 }
--- src/test/java/Problem01Test.java
+++ src/test/java/Problem01Test.java
@@ -1,2 +1,39 @@
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
 public class Problem01Test {
+    private Problem01 problem01;
+
+    @Before
+    public void setup() {
+        problem01 = new Problem01();
+    }
+
+    @Test
+    public void primeNumbersBefore10() {
+        String expected = "2 3 5 7";
+
+        String actual = problem01.computeAllPrimesSmallerThanEqualToN(10);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void primeNumbersBefore20() {
+        String expected = "2 3 5 7 11 13 17 19";
+
+        String actual = problem01.computeAllPrimesSmallerThanEqualToN(20);
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void primeNumbersBefore100() {
+        String expected = "2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97";
+
+        String actual = problem01.computeAllPrimesSmallerThanEqualToN(100);
+
+        assertEquals(expected, actual);
+    }
 }
