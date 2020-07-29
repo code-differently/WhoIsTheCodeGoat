3f547b53cd75e07dac9e1482d37f509d920fa20d
Brandon Daughtry <bdaughtr@aggies.ncat.edu>
2020-07-29 19:46:02 -0400

    problem1 passed

--- src/main/java/Problem01.java
+++ src/main/java/Problem01.java
@@ -1,2 +1,25 @@
 public class Problem01 {
+
+    public static String Problem01(int n){
+        String primes = "";
+        if(n>=2)
+            primes += "2 ";
+        for(int i=3; i<n; i+=2){
+            if(Problem01.Prime(i))
+                primes += Integer.toString(i) + " ";
+        }
+        return primes;
+    }
+
+    public static boolean Prime(int n){
+        int trash = 0;
+        if(n==2)
+            return true;
+        for(int i=2; i<n; i++)
+            if(n%i == 0) {
+                return false;
+            }
+        return true;
+    }
+
 }
