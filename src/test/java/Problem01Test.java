37b1156d4437afd4dafde285ffb8cd7a1232d6c2
Aldo <aalmeidarojo18@students.desu.edu>
2020-07-29 18:37:37 -0400

    problem 1 completed

--- src/test/java/Problem01Test.java
+++ src/test/java/Problem01Test.java
@@ -1,2 +1,53 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem01Test {
+
+    @Test
+    public void smallPrimeTest(){
+        // Given
+        String expected="2 3 5 7 11 13 17 19";
+
+        // When
+        String actual=Problem01.smallerPrimes(20);
+
+        // Then
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void smallPrimeTest2(){
+        // Given
+        String expected="2 3 5 7";
+
+        // When
+        String actual=Problem01.smallerPrimes(10);
+
+        // Then
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void smallPrimeTest3(){
+        // Given
+        String expected="2 3 5 7 11 13 17 19 23 29 31 37 41 43 47";
+
+        // When
+        String actual=Problem01.smallerPrimes(50);
+
+        // Then
+        Assert.assertEquals(expected,actual);
+    }
+
+    @Test
+    public void smallPrimeTest4(){
+        // Given
+        String expected="2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 67 71 73 79 83 89 97";
+
+        // When
+        String actual=Problem01.smallerPrimes(100);
+
+        // Then
+        Assert.assertEquals(expected,actual);
+    }
 }
