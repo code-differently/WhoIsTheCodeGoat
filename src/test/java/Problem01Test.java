e076cb4f72ab80d852cfde91b366c9dd75a860cf
KJ Witherspoon <50391760+Spooncodes@users.noreply.github.com>
2020-07-29 19:49:04 -0400

    NumeroUnoTheActivist

--- src/test/java/Problem01Test.java
+++ src/test/java/Problem01Test.java
@@ -1,2 +1,29 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem01Test {
+
+    @Test
+    public void Problem01Test1()
+    {
+        int n = 10;
+
+        boolean expected = true;
+        boolean  actual = Problem01.Prime(n);
+
+        Assert.assertTrue(true);
+    }
+
+    @Test
+    public void Problem01Test2(){
+
+        int n = 20;
+
+        boolean expected = true;
+        boolean actual = Problem01.Prime(n);
+
+        Assert.assertTrue(true);
+}
+
+
 }
