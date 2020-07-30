ba11d2660c337939a96dcb346e1f51a1c46d7ca1
Brandon Daughtry <bdaughtr@aggies.ncat.edu>
2020-07-29 22:32:10 -0400

    problem04 passed

--- src/main/java/Problem04.java
+++ src/main/java/Problem04.java
@@ -1,2 +1,18 @@
 public class Problem04 {
+
+    public static int[] productArrayPuzzle(int[] inputArray){
+        int[] productArray = new int[inputArray.length];
+        productArray[0] = 1;
+
+        for(int i=0; i<inputArray.length; i++){
+            productArray[0] *= inputArray[i];
+        }
+        for(int j=1; j<inputArray.length; j++){
+            productArray[j] = productArray[0];
+        }
+        for(int k=0; k<inputArray.length; k++){
+            productArray[k] /= inputArray[k];
+        }
+        return productArray;
+    }
 }
--- src/test/java/Problem04Test.java
+++ src/test/java/Problem04Test.java
@@ -1,2 +1,31 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem04Test {
+
+    @Test
+    public void Problem04Test1(){
+        //Given
+        int[] input = {10,3,5,6,2};
+
+        //Expected
+        int[] expected = {180,600,360,300,900};
+        int[] actual = Problem04.productArrayPuzzle(input);
+
+        //
+        Assert.assertArrayEquals(expected,actual);
+    }
+
+    @Test
+    public void Problem04Test2(){
+        //Given
+        int[] input = {20,12};
+
+        //Expected
+        int[] expected = {12,20};
+        int[] actual = Problem04.productArrayPuzzle(input);
+
+        //
+        Assert.assertArrayEquals(expected,actual);
+    }
 }
