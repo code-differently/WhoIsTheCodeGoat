f63a64d7d881f01bcff7f2f246bd13416c0cc505
brianawilliams <williamsb16@comcast.net>
2020-07-30 09:35:45 -0400

    Still working on this

--- src/main/java/Problem02.java
+++ src/main/java/Problem02.java
@@ -1,2 +1,34 @@
 public class Problem02 {
+
+    public static void main(String[] args){
+
+        //method that adds values into the magicSquare
+        int num = 3;
+        int i = 1;
+        int[][] magicSquare = new int[num][num];
+        int end = num^2;
+        int count = 0;
+
+        for(int row = 0; row < magicSquare.length; row++){
+            for(int column = 0; column < magicSquare[row].length; column++){
+                magicSquare[row][column] += i;
+                i++;
+                System.out.print(magicSquare[row][column] + "\t");
+            }
+            System.out.println();
+
+        }
+
+        //method to add the values in each row
+        for(int row = 0; row < magicSquare.length; row++){
+            int rowTotal = 0;
+            for(int column = 0; column < magicSquare.length; column++){
+                rowTotal = rowTotal + magicSquare[row][column];
+
+            }
+            System.out.println("Sum of each row "+ (row+1) + "is: " + rowTotal);
+        }
+
+
+    }
 }
--- src/test/java/Problem01Test.java
+++ src/test/java/Problem01Test.java
@@ -1,2 +1,34 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem01Test {
+
+
+    @Test
+    public void allPrimeNumbersTest01(){
+        //Given
+        Problem01 problem01 = new Problem01();
+        String expected = "2 3 5 7";
+
+        //When
+        String actual = problem01.allPrimeNumbers(10);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+
+    }
+
+    @Test
+    public void allPrimeNumbersTest02(){
+        //Given
+        Problem01 problem01 = new Problem01();
+        String expected = "2 3 5 7 11 13 17 19";
+
+        //When
+        String actual = problem01.allPrimeNumbers(20);
+
+        //Then
+        Assert.assertEquals(expected, actual);
+
+    }
 }
