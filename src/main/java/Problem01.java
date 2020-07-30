4d3725ae532f68dbe36d154e16ef8a5c06b3d5ef
IsaiahB1 <isaiahbarnett16@yahoo.com>
2020-07-30 09:02:21 -0500

    didnt finish but you said make pull request

--- src/main/java/Problem01.java
+++ src/main/java/Problem01.java
@@ -1,2 +1,26 @@
 public class Problem01 {
+    public static boolean checkPrimes(int n) {
+        //String nums = "";
+
+        if (n <= 1)
+            return false;
+
+        for (int i = 2; i < n; i++) {
+            if (n % i == 0) {
+                return false;
+            }
+        } return true;
+    }
+
+
+    public static String printPrimes(int n){
+        String nums = "";
+
+        for(int i = 2; i <= n; i++){
+            if(checkPrimes(i)){
+                nums += i + " ";
+            }
+        }
+        return nums;
+    }
 }
--- src/test/java/Problem02Test.java
+++ src/test/java/Problem02Test.java
@@ -1,2 +1,18 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
+
 public class Problem02Test {
+
+    @Test
+    public void problem02Test(){
+        Problem02 problem02 = new Problem02();
+
+        String expected = " 2   7   6\n" +
+                "  9   5   1\n" +
+                "  4   3   8\n";
+        String actual = problem02.makeSquare(3);
+
+        Assert.assertEquals(expected, actual);
+    }
+
 }
