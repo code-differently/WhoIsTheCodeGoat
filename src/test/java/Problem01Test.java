7033b2ec3bc7aac23e06cc5fad00651638409935
Malon Holmes <itsmalonholmes@gmail.com>
2020-07-30 09:24:58 -0400

    Didn't get the chance to really get far because of family stuff

--- src/test/java/Problem01Test.java
+++ src/test/java/Problem01Test.java
@@ -1,2 +1,31 @@
+import org.junit.Assert;
+import org.junit.Test;
+
 public class Problem01Test {
+
+    @Test
+    public void primesTest1(){
+        // Given
+        String expected = "2 3 5 7";
+
+        // When
+        String actual = Problem01.primes(10);
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void primesTest2(){
+        // Given
+        String expected = "";
+
+        // When
+        String actual = Problem01.primes(0);
+
+        // Then
+        Assert.assertEquals(expected, actual);
+    }
+
+
 }
