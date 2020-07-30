1e9c44666756d4b1100c81f7585f694535b2e506
Taia Williams-Rivera <tmwilliams181@students.desu.edu>
2020-07-29 21:34:43 -0400

    Completed problem 1, unedited pom file

--- src/main/java/Problem01.java
+++ src/main/java/Problem01.java
@@ -1,2 +1,51 @@
-public class Problem01 {
+import java.util.ArrayList;
+
+
+public class Problem01
+{   
+    public ArrayList <Integer>  sieveOfEratosthenes( int n )
+    {   
+        ArrayList <Integer> primesList = new ArrayList <Integer>();
+
+        for (int i = 2; i <= n; i++)
+        {
+            if(isPrime(i))
+            {
+                primesList.add(i);
+            }
+        }
+        return primesList;
+        
+        //primesArray = primesList.toArray();
+        //return primesArray;
+
+    }
+
+    public static boolean isPrime(int num) {
+        if (num <= 1) {
+            return false;
+        }
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                return false;
+            }
+        }
+        return true;
+ }
+
+
+public static void main(String[] args)
+{   
+    Problem01 problem01 = new Problem01();
+    int n = 10;
+
+        //when
+        problem01.sieveOfEratosthenes(n);
+        
+        //Object [] expected =  {2, 3, 5, 7};
+
+        //then
+        //assertArrayEquals(expected, actual);
+}
+
 }
