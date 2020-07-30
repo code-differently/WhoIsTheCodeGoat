657a15fe57f02f7fdec2c770f0855d7276d5759a
girlswhocodee <myekini1@umbc.edu>
2020-07-30 09:45:08 -0400

    Pushed little code completed

--- src/main/java/Problem01.java
+++ src/main/java/Problem01.java
@@ -1,2 +1,18 @@
 public class Problem01 {
+
+    public static boolean isPrimeNumber(int n){
+        if(n <= 1)
+        return false;
+        if(n <= 3)
+        return true;
+
+        for(int i = 2; i < n; i++)
+            if(n % 1 == 0)
+            return false;
+
+        return true;
+
+
+       
+    }
 }
--- src/main/java/Problem03.java
+++ src/main/java/Problem03.java
@@ -1,2 +1,22 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class Problem03 {
+
+    public static int countSubstring(String str, int K){
+        int num = str.length();
+        int answer = 0;
+
+
+        Map<Character, Integer> map = new HashMap<Character, Integer>();
+        for(int i = 0; i < K; i++){
+            if(map.get(str.charAt(i)) == null){
+                map.put(str.charAt(i), 1);
+            }
+            else {
+                map.put(str.charAt(i), map.get(str.charAt(i)) + 1);
+            }
+        }
+        return ;
+    }
 }
