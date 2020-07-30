96e9788f1dd064ec86d06675269bd2944f3fee3f
Juliana Paul <paul.juliana15@gmail.com>
2020-07-30 14:28:57 -0400

    Even more progress

--- src/main/java/Problem05.java
+++ src/main/java/Problem05.java
@@ -1,4 +1,23 @@
 public class Problem05 {
 
-    
+    public static String BiggerIsGreater(String word){
+
+        //steps ONE
+        // get every string variation of a string (you want permuatatiosn)
+        // every single possible combination using the characters of the given you inputted 
+
+
+        //Step TWO
+        // store all strings and put into a data structure other than a set to keep all of scambled words that you have
+
+
+        //Step THREE
+        //compare ORIGINAL GIVEN STRING and compare the strings from STEP TWO using the .compareTO() METHOD
+        //the scrambled string that lexigraphically GREATER than your ORGINAL string are added to new data structure "GREATER THAN WORD"
+
+
+        //Step four 
+        //find the smallest lecigraphic word in "GREATER THAN WORD" data structure 
+
+    }
 }
